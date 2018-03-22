a77629ec974843d05adbdab6c0e59d9fe554b493
https://github.com/PatriziaT <patrizia.trammell@yahoo.com>
2018-03-21 22:28:57 -0700

    header and footer

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -8,16 +8,22 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppComponent } from './app.component';
 
 import 'hammerjs';
+
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { DatePipe } from '@angular/common';
 
 import { DishService } from './services/dish.service';
+
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
